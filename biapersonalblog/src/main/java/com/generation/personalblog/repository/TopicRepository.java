aa6d725bbc3c24df0c247e1f02ff8299d77304ef
Kakubia <kakubia@gmail.com>
2022-05-13 19:46:26 -0300

    Corrected error in annotation on Topic Repository

--- biapersonalblog/src/main/java/com/generation/personalblog/repository/TopicRepository.java
+++ biapersonalblog/src/main/java/com/generation/personalblog/repository/TopicRepository.java
@@ -3,8 +3,11 @@ package com.generation.personalblog.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.personalblog.model.Topic;
+
+@Repository
 
 public interface TopicRepository extends JpaRepository<Topic, Long>{
 
