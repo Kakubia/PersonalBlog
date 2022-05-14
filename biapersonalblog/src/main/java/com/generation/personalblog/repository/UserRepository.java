d0fb17cd56ff803edec32f66c1148d49b64869fe
Kakubia <kakubia@gmail.com>
2022-05-14 15:21:08 -0300

    Inserted @Repository

--- biapersonalblog/src/main/java/com/generation/personalblog/repository/UserRepository.java
+++ biapersonalblog/src/main/java/com/generation/personalblog/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.generation.personalblog.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.personalblog.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	public Optional<User> findByUser(String user);
